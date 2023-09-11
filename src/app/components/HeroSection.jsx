ee17c4b6933230b612d5569af3ef98daefe8738f
Saoa35 <saoamah@gmail.com>
2023-09-11 16:53:48 +0400

    + installed react-type-animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,31 +4,32 @@ import Image from "next/image";
 export const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I`m Oleksandr
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-900">
+              Hello, I`m{" "}
+            </span>
+            Oleksandr
           </h1>
-          <p className="text-[#ADB7BE] text-base mb-6 lg:text-lg">
-            I am a Front End Web developer with 1 year experience in web
-            developing, which includes personal pet projects and practical
-            commercial experience in IT company, where I participated in the
-            development of different projects in a team of professional
-            developers.
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Front End developer with 1 year experience in web developing.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-300 text-black">
+            <button className="px-6 py-3 rounded-full w-[70%] sm:w-fit mr-4 bg-gradient-to-br from-blue-400 to-blue-900 hover:bg-slate-600 text-white">
               Hire Me
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white">
-              Download CV
+            <button className="mt-3 px-1 py-1 rounded-full w-[70%] sm:w-fit bg-gradient-to-br from-blue-400 to-blue-900 hover:bg-slate-800 text-white">
+              <span className="block bg-[#121212] hover:bg-slate-700 rounded-full px-5 py-2">
+                Download CV
+              </span>
             </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/Portfolio-Avatar.png"
+              src="/images/hacker.png"
               alt="Portfolio Avatar"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
