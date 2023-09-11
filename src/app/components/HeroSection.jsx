ba76fea611f3e714efc820e4a55ea6f9e3045c2a
Saoa35 <saoamah@gmail.com>
2023-09-11 13:44:10 +0400

    + finished HeroSection comp.

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import Image from "next/image";
 
 export const HeroSection = () => {
   return (
     <section>
-      <h1 className="text-white">Hello, I`m Oleksandr</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I`m Oleksandr
+          </h1>
+          <p className="text-[#ADB7BE] text-base mb-6 lg:text-lg">
+            I am a Front End Web developer with 1 year experience in web
+            developing, which includes personal pet projects and practical
+            commercial experience in IT company, where I participated in the
+            development of different projects in a team of professional
+            developers.
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-300 text-black">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white">
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/Portfolio-Avatar.png"
+              alt="Portfolio Avatar"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
