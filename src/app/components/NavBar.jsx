5c2d5844c6b6479ab001cdffa8443c2e939c2a82
Saoa35 <saoamah@gmail.com>
2023-09-12 11:08:40 +0400

    + installed heroicons

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -20,13 +20,13 @@ const navLinks = [
 
 export const NavBar = () => {
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link className="text-5xl text-white font-semibold" href="/">
-          LOGO
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link className="text-lg md:text-2xl text-white font-semibold" href="/">
+          My portfolio
         </Link>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex">
+          <ul className="flex md:flex-row p-4 md:p-0 md:space-x-8 mt-0">
             {navLinks.map((link, id) => (
               <li key={id}>
                 <NavLink href={link.path} title={link.title} />
