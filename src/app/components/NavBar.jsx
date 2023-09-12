c302391953b91c5f924f2af38ce11924bca04044
Saoa35 <saoamah@gmail.com>
2023-09-12 11:57:11 +0400

    + added Bars3Icon

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { NavLink } from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const navLinks = [
   {
@@ -19,12 +21,23 @@ const navLinks = [
 ];
 
 export const NavBar = () => {
+  const [navBarOpen, setNavBarOpen] = useState(true);
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
       <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <Link className="text-lg md:text-2xl text-white font-semibold" href="/">
           My portfolio
         </Link>
+        <div className="mobile-menu block md:hidden">
+          {navBarOpen ? (
+            <button className="text-slate-200 flex items-center px-3 py-2 border rounded">
+              <Bars3Icon className="w-5 h-5" />
+            </button>
+          ) : (
+            <button></button>
+          )}
+        </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex md:flex-row p-4 md:p-0 md:space-x-8 mt-0">
             {navLinks.map((link, id) => (
