c82ecc02e9941651b9d69dc94bfb7b30ce58e829
Saoa35 <saoamah@gmail.com>
2023-09-12 12:43:08 +0400

    + created AboutSection comp.

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { NavLink } from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { MenuOverlay } from "./MenuOverlay";
 
 const navLinks = [
   {
@@ -24,7 +25,7 @@ export const NavBar = () => {
   const [navBarOpen, setNavBarOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <Link className="text-lg md:text-2xl text-white font-semibold" href="/">
           My portfolio
@@ -56,6 +57,7 @@ export const NavBar = () => {
           </ul>
         </div>
       </div>
+      {navBarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   );
 };
