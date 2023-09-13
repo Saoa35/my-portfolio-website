6f6d5ba3c1a0b9ab426c41ead1bfe3081c0b672f
Saoa35 <saoamah@gmail.com>
2023-09-13 16:29:48 +0400

    + added inputs & textarea

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -17,20 +17,78 @@ export const EmailSection = () => {
           suggestion, want to say Hi, or offer me a job, I would be glad to
           contact with you.
         </p>
-        <div className="socials flex flex-row gap-2">
+        <div className="socials flex flex-row gap-6">
           <Link href="https://github.com/Saoa35">
-            <Image src={GithubIcon} alt="Github Icon" />
+            <Image src={GithubIcon} alt="Github Icon" width={40} height={40} />
           </Link>
           <Link href="https://join.skype.com/invite/Akb6lZbgAL2D">
-            <Image src={SkypeIcon} alt="Skype Icon" />
+            <Image src={SkypeIcon} alt="Skype Icon" width={40} height={40} />
           </Link>
           <Link href="https://t.me/Alexandr_Makharadze">
-            <Image src={TelegramIcon} alt="Telegram Icon" />
+            <Image
+              src={TelegramIcon}
+              alt="Telegram Icon"
+              width={40}
+              height={40}
+            />
           </Link>
           <Link href="https://www.linkedin.com/in/oleksandr-makharadze/">
-            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+            <Image
+              src={LinkedinIcon}
+              alt="Linkedin Icon"
+              width={40}
+              height={40}
+            />
           </Link>
         </div>
+      </div>
+      <div>
+        <form className="flex flex-col">
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="text-white block text-sm font-medium mb-2"
+            >
+              Your email
+            </label>
+            <input
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              type="email"
+              id="email"
+              required
+              placeholder="youremail@mail.com"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="subject"
+              className="text-white block text-sm font-medium mb-2"
+            >
+              Subject
+            </label>
+            <input
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              type="text"
+              id="subject"
+              required
+              placeholder="Hello..."
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="text-white block text-sm font-medium mb-2"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Message content..."
+            />
+          </div>
+        </form>
       </div>
     </section>
   );
