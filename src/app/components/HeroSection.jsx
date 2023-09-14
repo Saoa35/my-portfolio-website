8ab0bd87ac62fd7d38e6b6e179f4a381041dd0e9
Saoa35 <saoamah@gmail.com>
2023-09-14 12:04:04 +0400

    + added animation to HeroSection

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,7 +9,12 @@ export const HeroSection = () => {
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left sm:mr-4">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3 }}
+          className="col-span-7 place-self-center text-center sm:text-left sm:mr-4"
+        >
           <h1 className="text-white h-28 sm:h-36 lg:h-44 xl:h-fit mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="block text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-900">
               Hello, I`m{" "}
@@ -33,17 +38,22 @@ export const HeroSection = () => {
             them into live is a passion.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full w-[70%] sm:w-fit mr-4 bg-gradient-to-br from-blue-400 to-blue-900 hover:opacity-90 hover:scale-110 text-white">
+            {/* <button className="px-6 py-3 rounded-full w-[70%] sm:w-fit mr-4 bg-gradient-to-br from-blue-400 to-blue-900 hover:opacity-90 hover:scale-110 text-white">
               Hire Me
-            </button>
+            </button> */}
             <button className="mt-3 px-1 py-1 rounded-full w-[70%] sm:w-fit bg-gradient-to-br from-blue-400 to-blue-900 hover:scale-110 hover:bg-slate-900 text-white">
               <span className="block bg-[#121212] hover:bg-slate-700 rounded-full px-5 py-2">
                 Download CV
               </span>
             </button>
           </div>
-        </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3 }}
+          className="col-span-5 place-self-center mt-4 lg:mt-0"
+        >
           <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hacker.png"
@@ -53,7 +63,7 @@ export const HeroSection = () => {
               height={300}
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
