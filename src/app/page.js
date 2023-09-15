d5d912ad06a5c4fe35b3f57c181edd05ef70a8d9
Saoa35 <saoamah@gmail.com>
2023-09-15 11:51:52 +0400

    + created ThemeProvider

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,21 @@ import { AboutSection } from "./components/AboutSection";
 import { ProjectsSection } from "./components/ProjectsSection";
 import { EmailSection } from "./components/EmailSection";
 import { Footer } from "./components/Footer";
+import { ThemeProvider } from "./context/ThemeContext";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <NavBar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
+    <ThemeProvider>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <NavBar />
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+        </div>
+        <Footer />
+      </main>
+    </ThemeProvider>
   );
 }
