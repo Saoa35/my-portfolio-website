cf5f6a800d45bd52aecea51a4385f0545bf4e25f
Saoa35 <saoamah@gmail.com>
2023-09-15 11:34:36 +0400

    + added handleEmailClick func.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,11 +1,23 @@
+"use client";
+
 import React from "react";
 
 export const Footer = () => {
+  const handleEmailClick = (e) => {
+    e.preventDefault();
+
+    window.location.href = "mailto:saoamah@gmail.com";
+  };
+
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex flex-col items-center sm:flex-row sm:justify-between ">
+      <div className="container p-16 gap-2 sm:gap-0 flex flex-col items-center sm:flex-row sm:justify-between ">
         <span className="text-slate-300">@ Web development 2023</span>
-        <a href="mailto:saoamah@gmail.com" className="text-blue-900">
+        <a
+          href="mailto:saoamah@gmail.com"
+          className="text-blue-900"
+          onClick={handleEmailClick}
+        >
           saoamah@gmail.com
         </a>
       </div>
