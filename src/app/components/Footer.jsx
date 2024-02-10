13f619a9c9cd96e5a4c7cff9eb6f5f9c78739431
Saoa35 <saoamah@gmail.com>
2024-02-10 16:12:44 +0100

    * added new project

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -12,7 +12,7 @@ export const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-16 gap-2 sm:gap-0 flex flex-col items-center sm:flex-row sm:justify-between ">
-        <span className="text-slate-300">@ Web development 2023</span>
+        <span className="text-slate-300">@ Web development 2024</span>
         <a
           href="mailto:saoamah@gmail.com"
           className="text-blue-900"
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -8,13 +8,13 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "Travel-Blog",
+    title: "Ecommerce",
     description:
-      "This is the React, NodeJs fullstack project implements registration, authorization, writing an article, adding photos to it, with the ability to edit the article, also you can add likes and comments.",
-    image: "/images/projects/travel-blog.png",
+      "This is NextJs ecommerce project. Implemented displaying of a list of products, selection and viewing of a specific product, as well as the appearance of a modal window with a description of the product.",
+    image: "/images/projects/ecommerce.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/Saoa35/Travel-Blog",
-    previewUrl: "/",
+    gitUrl: "https://github.com/Saoa35/ecommere-marketplace",
+    previewUrl: "https://ecommerce-iota-cyan.vercel.app/",
   },
   {
     id: 2,
@@ -28,16 +28,28 @@ const projectsData = [
   },
   {
     id: 3,
-    title: "Burger Lord",
+    title: "Travel App",
     description:
-      "This is Burger restaurant website. It was created on Reactjs. Implemented the ability to select a burger, plus an addition to it, also implemented a basket, where the number of selected burgers and the total amount of the selected product are counted, sorting and search are also implemented.",
-    image: "/images/projects/burger-lord.png",
+      "This is a simple React project, created with using TypeScript, Redux, RestAPI... where you can register or signin and choose travel wich you would prefer.",
+    image: "/images/projects/travel-app.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/Saoa35/burger_lord",
-    previewUrl: "/",
+    gitUrl:
+      "https://bitbucket.org/bineducation/lecture-starter-redux/src/master/",
+    previewUrl:
+      "https://lecture-starter-redux-2p9bx9fb7-oleksandr-makharadze.vercel.app/sign-in",
   },
   {
     id: 4,
+    title: "Photo gallery",
+    description:
+      "Photo gallery website. You can select a photo and open it in a modal window in a larger size, also implemented searching and sorting by resort names. ",
+    image: "/images/projects/galery.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Saoa35/photo_gallery",
+    previewUrl: "https://photo-gallery-saoa35.vercel.app/",
+  },
+  {
+    id: 5,
     title: "Marketplace",
     description:
       "This is React Native mobile app marketplace when you can Sign Up or Sign In, after authorization you can see all the products, also you can add your own product and delete it, you can change your profile settings which include avatar, name and phone number.",
@@ -46,16 +58,16 @@ const projectsData = [
     gitUrl: "https://github.com/Saoa35/marketplace-mobile-app",
     previewUrl: "/",
   },
-  {
-    id: 5,
-    title: "Simple Chat",
-    description:
-      "This is a simple NodeJs, Socket.IO chat project, where you can register, and participate in simple conversation with other users. Multiple users can participate in a conversation at the same time.",
-    image: "/images/projects/chat1.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/Saoa35/Simple_Chat",
-    previewUrl: "/",
-  },
+  // {
+  //   id: 5,
+  //   title: "Simple Chat",
+  //   description:
+  //     "This is a simple NodeJs, Socket.IO chat project, where you can register, and participate in simple conversation with other users. Multiple users can participate in a conversation at the same time.",
+  //   image: "/images/projects/chat1.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "https://github.com/Saoa35/Simple_Chat",
+  //   previewUrl: "/",
+  // },
   {
     id: 6,
     title: "MyJornyFinances",
@@ -68,13 +80,13 @@ const projectsData = [
   },
   {
     id: 7,
-    title: "Photo gallery",
+    title: "Burger Lord",
     description:
-      "Photo gallery website. You can select a photo and open it in a modal window in a larger size, also implemented searching and sorting by resort names. ",
-    image: "/images/projects/galery.png",
+      "This is Burger restaurant website. It was created on Reactjs. Implemented the ability to select a burger, plus an addition to it, also implemented a basket, where the number of selected burgers and the total amount of the selected product are counted, sorting and search are also implemented.",
+    image: "/images/projects/burger-lord.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/Saoa35/photo_gallery",
-    previewUrl: "https://photo-gallery-saoa35.vercel.app/",
+    gitUrl: "https://github.com/Saoa35/burger_lord",
+    previewUrl: "/",
   },
   {
     id: 8,
@@ -88,13 +100,13 @@ const projectsData = [
   },
   {
     id: 9,
-    title: "Ecommerce",
+    title: "Travel-Blog",
     description:
-      "This is NextJs ecommerce project. Implemented displaying of a list of products, selection and viewing of a specific product, as well as the appearance of a modal window with a description of the product.",
-    image: "/images/projects/ecommerce.png",
+      "This is the React, NodeJs fullstack project implements registration, authorization, writing an article, adding photos to it, with the ability to edit the article, also you can add likes and comments.",
+    image: "/images/projects/travel-blog.png",
     tag: ["All", "Web"],
-    gitUrl: "https://github.com/Saoa35/ecommere-marketplace",
-    previewUrl: "https://ecommerce-iota-cyan.vercel.app/",
+    gitUrl: "https://github.com/Saoa35/Travel-Blog",
+    previewUrl: "/",
   },
 ];
 
